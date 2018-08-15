de097e47dd0e7a5fa48dd28b140752cb658ca28e
Alex Kelly <alexkelly51@gmail.com>
2018-08-15 16:25:42 +0100

    created a restaurant app where you can add and review resturants and click through to them

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# restaurant = Restaurant.create([{}])
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  restaurant_name = Faker::TheITCrowd.character
+  restaurant_address = Faker::Pokemon.location
+  restaurant_phone_number = Faker::PhoneNumber.cell_phone
+  restaurant_category = categories.sample
+
+  restaurant = Restaurant.new(name: restaurant_name, address: restaurant_address, phone_number: restaurant_phone_number, category: restaurant_category)
+  restaurant.save
+end
+
+puts 'Finished!'
